418a10da6cd02ecb6baed47d118b528d57b98d95
Krzysztof Wawer <krzysztof.wawer@gmail.com>
2023-08-17 13:25:59 +0200

    Update README.md, add test

--- README.md
+++ README.md
@@ -5,6 +5,13 @@
 This repo contains programming exercises in Elixir language.
 
 - [Project Euler](https://projecteuler.net/)
+- [Advent of Code](https://adventofcode.com/)
+
+## Check code style
+
+```
+mix credo --all
+```
 
 ## Verification
 
@@ -12,4 +19,10 @@ Every exercise contains test written in ExUnit.
 
 ```
 mix test
+```
+
+### Check test coverage
+
+```
+mix test --cover
 ```
--- test/exercises/advent_of_code/aoc2015/day01/part2/part2_test.exs
+++ test/exercises/advent_of_code/aoc2015/day01/part2/part2_test.exs
@@ -2,6 +2,10 @@ defmodule AoC2015Day01Part2Test do
   use ExUnit.Case
   doctest AoC2015Day01Part2
 
+  test "with input (" do
+    assert AoC2015Day01Part2.run("(") == -1
+  end
+
   test "with input ()())" do
     assert AoC2015Day01Part2.run("()())") == 5
   end
@@ -10,7 +14,7 @@ defmodule AoC2015Day01Part2Test do
     assert AoC2015Day01Part2.run(")") == 1
   end
 
-  test "with input big input" do
+  test "with big input" do
     {_, input} = File.read("test/support/advent_of_code/2015/day_01/input.txt")
     assert AoC2015Day01Part2.run(input) == 1_795
   end
